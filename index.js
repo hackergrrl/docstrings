fab690464f97c8d2c5d0869bc87ad16b29c10a69
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-08-15 10:26:34 -0700

    handle use strict

--- index.js
+++ index.js
@@ -2,31 +2,59 @@ var esprima = require('esprima')
 
 module.exports = function (f) {
   "Interpret a string literal at the beginning of a function as its documentation."
-  
+
   if (typeof f !== 'function') {
     throw new TypeError('not a function')
   }
 
   var res
-  var expr = esprima.parse(f.toString()).body[0].body
-  if (expr && expr.body && expr.body[0] && expr.body[0].expression) {
-    expr = expr.body[0].expression
-  } else {
+  var body = esprima.parse(f.toString()).body[0].body
+  var expr = getNthExpression(body, 0)
+
+  if (!expr || !isExpressionStringLiteral(expr)) {
     return ''
   }
 
-  switch (expr.type) {
-    case 'Literal':
-      res = expr.value
-      break
-    case 'TemplateLiteral':
-      res = expr.quasis[0].value.raw
-      break
-    default:
+  // Skip 'use strict' and look at the next expression
+  var value = getStringLiteralValue(expr)
+  if (value === 'use strict') {
+    expr = getNthExpression(body, 1)
+    if (!expr || !isExpressionStringLiteral(expr)) {
       return ''
+    }
+    value = getStringLiteralValue(expr)
   }
 
-  return res
+  return value
     .replace(/ +/g, ' ')
     .replace(/^\s*/mg, '')
+}
+
+function getNthExpression (body, n) {
+  "Given the esprima-parsed body of a function, retrieve the Nth expression."
+
+  if (body && body.body && body.body[n] && body.body[n].expression) {
+    return body.body[n].expression
+  } else {
+    return null
+  }
+}
+
+function isExpressionStringLiteral (expr) {
+  "Determines if the given expression is a string literal."
+
+  return expr.type === 'Literal' || expr.type === 'TemplateLiteral'
+}
+
+function getStringLiteralValue (expr) {
+  "Pulls out the string value of a string literal expression."
+
+  switch (expr.type) {
+    case 'Literal':
+      return expr.value
+    case 'TemplateLiteral':
+      return expr.quasis[0].value.raw
+    default:
+      throw new Error('expr is not a string literal')
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var ds = require('../index')
 var test = require('tape')
 
-test(function (t) {
+test('basics', function (t) {
   function foo () {
     "returns foo"
     return 'foo'
@@ -22,12 +22,32 @@ test(function (t) {
      quux"
   }
 
+  function withWhitespace () {
+
+    "description"
+
+    return 0
+  }
+
   function none() {}
 
   t.equal(ds(foo), 'returns foo')
   t.equal(ds(bar), 'returns bar')
   t.equal(ds(baz), 'multiline comments\nneed love\ntoo')
   t.equal(ds(quux), 'Returns quux')
+  t.equal(ds(withWhitespace), 'description')
   t.equal(ds(none), '')
+  t.end()
+})
+
+test('use strict', function (t) {
+  function strictAdd2 (x, y) {
+    'use strict'
+    'Adds two numbers together and returns their sum.'
+
+    return x + y
+  }
+
+  t.equal(ds(strictAdd2), 'Adds two numbers together and returns their sum.')
   t.end()
 })
